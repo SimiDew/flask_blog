b03349e58037e3d5f9d38502f2079ff06eab14cc
Simi <simona.priya.s@gmail.com>
2017-10-14 15:09:47 +0100

    Initial commit

---
+++ flaskr.py
@@ -0,0 +1,152 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/about')
+def about():
+    if session.get('logged_in'):
+        msg = 'Hi your name is Simi'
+    else:
+        msg = 'Hi my name is Simi'
+    return render_template('about.html', msg=msg)
+
+
+@app.route('/guest_book')
+def guest_book():
+    # Shows guest book entries
+    db = get_db()
+    cur = db.execute('select title, text from guest_entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('guest_book.html', entries=entries)
+
+
+@app.route('/add_guest_entry', methods=['POST'])  # <----
+def add_guest_entry():
+    if not session.get('logged_in'):
+        pass  # unauthorised?
+    db = get_db()
+    db.execute('insert into guest_entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('guest_book'))
+
+
+@app.route('/search_entries', methods=['GET', 'POST'])
+def search_entries():
+    query = request.form['search_entries']
+    db = get_db()
+    cur = db.execute('select title, text from entries where title or text like "%'+query+'%" order by id desc')
+    entries = cur.fetchall()
+    return render_template('search_entries.html', entries=entries)
+
+"""
+
+- find a specific blog entry
+<input type="text" name="fname" placeholder="First name">
+
+
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+- search for blog entries containing a certain word
+- showing 20 blog entries per page
+
+"""
